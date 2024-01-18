33db6079ca82190ff961ef30755ad0f621ad46f5
Adan Gimenez <gimenezadan1@gmail.com>
2024-01-18 14:48:37 -0600

    updated

--- crud/settings.py
+++ crud/settings.py
@@ -77,28 +77,28 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    #     'USER': 'amusic2024',
-    #     'PASSWORD': 'amusic2024',
-    #     'NAME': 'adanined_amusic',
-    #     'OPTIONS': {
-    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-    #     }
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
         'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'Password-123456',
-        'NAME': 'amusic',
+        'USER': 'amusic2024',
+        'PASSWORD': 'amusic2024',
+        'NAME': 'adanined_amusic',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Password-123456',
+    #     'NAME': 'amusic',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    #     }
+    # }
 }
 
 pymysql.install_as_MySQLdb()
