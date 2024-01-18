d51dd52a7ed04941e21ec8d75594dd1741d5ba09
Adan Gimenez <gimenezadan1@gmail.com>
2024-01-18 14:52:27 -0600

    updated db

--- crud/settings.py
+++ crud/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
         'PORT': '3306',
-        'USER': 'amusic2024',
+        'USER': 'adanined_amusic',
         'PASSWORD': 'amusic2024',
         'NAME': 'adanined_amusic',
         'OPTIONS': {
