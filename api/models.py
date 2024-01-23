f7b7c4c9cefa55cbd055b1674af76a58dbd82cc4
Adan Gimenez <gimenezadan1@gmail.com>
2024-01-23 15:32:34 -0600

    new migrations

--- api/models.py
+++ api/models.py
@@ -3,6 +3,4 @@ from django.db import models
 # Create your models here.
 class PlayList(models.Model):
     id_user = models.IntegerField()
-    id_music = models.IntegerField()
-    singer_name = models.CharField(max_length=100)
-    song_name = models.CharField(max_length=100)
+    id_music_deezer = models.IntegerField()
