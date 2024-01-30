1920b64f2f10608a03c5400cdf557dd8d5a1df55
Adan Gimenez <gimenezadan1@gmail.com>
2024-01-30 16:18:36 -0600

    migration fiel

--- api/models.py
+++ api/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class PlayList(models.Model):
     id_user = models.IntegerField()
-    id_music_deezer = models.IntegerField()
+    id_music_deezer = models.BigIntegerField()
